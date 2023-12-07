02e653affb2ba610f634cfaba24034a98e0b99a7
Felipe <felipe.baleche2@gmail.com>
2023-12-07 12:06:20 -0300

    added user orm and small changes in h2 properties

---
+++ src/main/java/com/project/backend/entity/User.java
@@ -0,0 +1,80 @@
+package com.project.backend.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    //Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    //Builders
+    public User(){}
+
+    public User (String name, String email, String phone, String password){
+        setName(name);
+        setEmail(email);
+        setPhone(phone);
+        setPassword(password);
+    }
+
+    //Methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //Accessors
+    public Long getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
